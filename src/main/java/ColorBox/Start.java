e156108c9e1e7f74750572c1b41dc4300e643102
antonfrid <ki@ces.com>
2022-03-11 21:51:35 +0300

    edit String line #11

--- src/main/java/ColorBox/Start.java
+++ src/main/java/ColorBox/Start.java
@@ -8,7 +8,7 @@ public class Start {
     private JFrame window;
 
     public Start(){
-         window = new JFrame("ColorBox (Си-Би)");
+         window = new JFrame("ColorBox (Си-Би-би)");
          window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
          window.setSize(700, 500);
 
